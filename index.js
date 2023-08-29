0819de3d84abcccf24e24bc7bd3dd0b8b2fbf8ec
karunakar <kamalmudiraj039@gmail.com>
2023-08-29 11:13:37 +0000

    commiting react_super_over_league_app

---
+++ index.js
@@ -0,0 +1,20 @@
+const element = (
+  //   Write your code here.
+  <div className="main-container">
+    <h1 className="main-heading">Super Over League</h1>
+    <div className="sub-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+        height="200px"
+        width="200px"
+      />
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+        height="200px"
+        width="200px"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
